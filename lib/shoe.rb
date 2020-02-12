9f57b2f27f155399ef04642aa1bd1fe2b2f7d239
James Batsell <jamesbatsell05@gmail.com>
2020-02-12 04:27:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,10 +5,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
-  else BRANDS << brands
+    if BRANDS.include? (brand)
+    else BRANDS << brand
+    end
   end
-end
   
   def cobble
     self.condition = "new"
